41716ec5cf490917e0df4f557001e53d43ebfc46
RedJunKi <bing8821@naver.com>
2024-07-23 19:10:52 +0900

    프로젝트 A 기능구현 완료

--- src/main/java/com/example/fullstack_study_boostcourse/ViewController.java
+++ src/main/java/com/example/fullstack_study_boostcourse/ViewController.java
@@ -1,5 +1,6 @@
 package com.example.fullstack_study_boostcourse;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -25,10 +29,28 @@ public class ViewController {
     }
 
     @GetMapping("/aboutme/today")
-    public String today(Model model) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yy/MM/dd h:m");
+    @ResponseBody
+    public void today(HttpServletResponse response) throws IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<head>" +
+                "<meta charset=\"utf-8\">" +
+                "<title>현재시간</title>" +
+                "<style>" +
+                "body { text-align: center; }" +
+                "h1 { font-size: 3em; }" +
+                "</style>" +
+                "</head>");
+        writer.println("<body>");
+        writer.println("<a href=\"/\">메인메뉴</a>");
+
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yy/MM/dd hh:mm");
         String today = LocalDateTime.now().format(dateTimeFormatter);
-        model.addAttribute("today", today);
-        return "today.html";
+        writer.println("<h1>현재 시간 : " + today + "</h1>");
+        writer.println("</body>");
+        writer.println("</html>");
+        writer.close();
     }
 }
