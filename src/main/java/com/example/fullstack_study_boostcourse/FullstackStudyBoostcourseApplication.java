c4944b64df245d6d137a1e9956bd958235aab57c
RedJunKi <bing8821@naver.com>
2024-07-23 18:13:52 +0900

    first commit

---
+++ src/main/java/com/example/fullstack_study_boostcourse/FullstackStudyBoostcourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstack_study_boostcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackStudyBoostcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackStudyBoostcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fullstack_study_boostcourse/FullstackStudyBoostcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstack_study_boostcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackStudyBoostcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
